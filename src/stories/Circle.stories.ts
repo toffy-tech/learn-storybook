b4c5aaf549ce3e125b250576a1db9c4fd1750d07
TatSu <toffy.technology@gmail.com>
2024-01-14 12:39:25 +0900

    新しいカタログを追加

---
+++ src/stories/Circle.stories.ts
@@ -0,0 +1,11 @@
+import { Meta } from "@storybook/react";
+import Circle from "./Circle";
+
+const meta: Meta<typeof Circle> = {
+  component: Circle,
+  title: "Circle",
+};
+
+export default meta;
+
+export const BaseCircle = {};
