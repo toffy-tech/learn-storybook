475ec660e69888d3f92c7a031fb8c6622dcbc44f
TatSu <toffy.technology@gmail.com>
2024-01-14 13:58:35 +0900

    色を変更できる機能

--- src/stories/Circle.stories.ts
+++ src/stories/Circle.stories.ts
@@ -4,8 +4,27 @@ import Circle from "./Circle";
 const meta: Meta<typeof Circle> = {
   component: Circle,
   title: "Circle",
+  argTypes: {
+    variant: {
+      control: { type: "select" }, //デフォルトのラジオボタン→セレクトボタン
+    },
+  },
 };
 
 export default meta;
 
-export const BaseCircle = {};
+export const BaseCircle = {
+  args: {
+    variant: "orange",
+  },
+};
+export const GreenCircle = {
+  args: {
+    variant: "green",
+  },
+};
+export const YellowCircle = {
+  args: {
+    variant: "yellow",
+  },
+};
--- src/stories/Circle.tsx
+++ src/stories/Circle.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-const Circle = () => {
-  return <div className="bg-orange-500 w-14 h-14 p-2 rounded-full"></div>;
+type Props = {
+  variant: "orange" | "green" | "yellow";
+};
+
+const Circle = ({ variant }: Props) => {
+  let bgColor;
+  switch (variant) {
+    case "orange":
+      bgColor = "bg-orange-500";
+      break;
+    case "green":
+      bgColor = "bg-green-500";
+      break;
+    case "yellow":
+      bgColor = "bg-yellow-500";
+      break;
+  }
+  return <div className={`${bgColor} w-14 h-14 p-2 rounded-full`}></div>;
 };
 
 export default Circle;
